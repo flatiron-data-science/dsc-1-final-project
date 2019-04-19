e60a01ffc9868fac0c1d26f556e8a14ad4434f0f
Patrick Fuller <patrickbfuller@gmail.com>
2019-04-18 20:44:30 -0700

    templating possible make a linear coef dict and create a mock home df

--- feat_to_model_eval.py
+++ feat_to_model_eval.py
@@ -68,3 +68,67 @@ def predict_from_line(line_vector, predict_vector):
     print('This is probbbbbaaaabbbbllllyy the price for those variables')
     return sum(line_vector*predict_vector)
     
+
+def feat_to_model_kfold_eval(target: str, predictors:list, df, kvals: list, show_summary=False, price_logged=False, MAE=False):
+    '''Returns:
+            -line vector of type array
+            -list of residuals of type array
+            -root mean squared error
+            -mean absolute error if activated'''
+    
+    df = df[[target] + predictors]     # removing any features not included in model
+    
+    predictors_df = df.drop(target, axis=1)    
+    
+    f = "+".join(predictors_df.columns)    # This could be simplified to predictors instead predictors_df.columns but its the end of the day skip for now
+    f = target + "~" + f
+    model = ols(formula=f, data=df).fit()
+    if show_summary:
+        display(model.summary())
+    
+    y = df[target]
+    X = df.drop(target, axis=1)
+    
+    total_errs = np.array([])
+    k_vals = [5,10, 20]
+    for k in k_vals:
+        for train_index, test_index in KFold(n_splits=k).split(X):
+
+                X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+                y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+
+                linreg.fit(X_train, y_train)
+                y_hat = linreg.predict(X_test)
+                errs = y_test - y_hat
+                if price_logged:
+                    errs = np.exp(y_test) - np.exp(y_hat)
+
+                total_errs = np.concatenate((total_errs, errs))
+                
+                
+    linear_coefs = np.array(model.params)
+    
+    line = {'intercept': linear_coefs[0]}
+    for col, coef in zip(predictors, linear_coefs[1:]):
+        line[col] = coef
+    
+    rmse = np.sqrt(np.mean(total_errs**2))
+
+    if MAE:
+        mae = abs(total_errs).mean()
+        print(f'Line: {line}\n'
+             f'RMSE: {rmse}\n'
+             f'MAE: {mae}\n'
+             f'MAE DISPLAYED')
+        return line, total_errs, rmse, mae
+    else:
+        print(f'Line: {line}\n'
+             f'RMSE: {rmse}\n'
+             f'Please pass in MAE=True if you\'d like to see MAE')
+        return line, total_errs, rmse
+            
+def predict_from_line(line_vector, predict_vector):
+    '''Predict vector element unit 1 to multiply the intercept'''
+    print('This is probbbbbaaaabbbbllllyy the price for those variables')
+    return sum(line_vector*predict_vector)
+    
